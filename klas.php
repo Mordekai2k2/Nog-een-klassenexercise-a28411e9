b8ff2df1ce0ccd043b3b75620e2af8e55eeafce6
Randy Brouwer <2105070@talnet.nl>
2019-10-09 09:31:00 +0100

    Uitwerking van de exercise

---
+++ klas.php
@@ -0,0 +1,8 @@
+<?php
+$namen = readline("WIE ZIJN JE VRIENDEN, IK ZWEER JOU GA MIJ VERTELLEN!!! *holds at gunpoint*\n\n\n\n\n\n");
+PHP_EOL;
+$yeet = explode(" ", $namen);
+echo "Deaths list:\n";
+foreach ($yeet as $Feckers) {
+    echo "$Feckers\n";
+}
